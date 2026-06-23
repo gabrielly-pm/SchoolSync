8366f0eb7714bd9af178d8bfe94bd4fbac3e3b7f
Gabrielly <gaby.20pereiramachado@gmail.com>
2026-06-23 20:01:34 -0300

    adicionar controle de frequencia e relatorio anual

---
+++ Frequencia.java
@@ -0,0 +1,11 @@
+public class Frequencia {
+    int faltas;
+
+    public Frequencia(int faltas) {
+        this.faltas = faltas;
+    }
+
+    public int getFaltas() {
+        return faltas;
+    }
+}
