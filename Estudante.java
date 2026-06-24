e1ee88da5613b60341ebbabe48aeb2050f171f82
Gabrielly <gaby.20pereiramachado@gmail.com>
2026-06-23 21:33:10 -0300

    adicionar validação financeira do estudante

--- Estudante.java
+++ Estudante.java
@@ -1,9 +1,11 @@
 public class Estudante {
     String nome;
     int matricula;
+    boolean mensalidadeDia;
 
-    public Estudante(String nome, int matricula) {
+    public Estudante(String nome, int matricula, boolean mensalidade) {
         this.nome = nome;
         this.matricula = matricula;
+        this.mensalidadeDia = mensalidadeDia;
     }
 }
